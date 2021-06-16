Removing periodic BCs for 2 rotors, making the binning method the standard for averages and errors, fixing the ED method and fixing sweeps to match these changes

ChoiceMC.py
@@ -261,8 +261,11 @@ class ChoiceMC(object):
                         if m1==m1p and m2==m2p:
                             # Kinetic contribution
                             H[m1*size + m2, m1p*size + m2p] += B * float(m1**2)
+                            H[m1*size + m2, m1p*size + m2p] += B * float(m2**2)
                         # Potential contribution
                         H[m1*size + m2, m1p*size + m2p] += g * (-1*sin_mmp[m1,m1p]*sin_mmp[m2,m2p] - 2*cos_mmp[m1,m1p]*cos_mmp[m2,m2p])
+        
+        
         # Finding the eigenavalues and eigenvectors
         evals, evecs = np.linalg.eigh(H)
         # Evaluating the observables
@@ -706,12 +709,12 @@ class ChoiceMC(object):
                         # Interaction with the rotor to the left
                         for ir in range(len(prob_full)):
                             prob_full[ir]*=self.rhoVij[ir,path_phi[i-1,p]]
-                    if (i==0):
-                        # Periodic BC for the leftmost rotor
+                    if (i==0) and (self.N>2):
+                        # Periodic BC for the leftmost rotor, doesn't apply to the 2 rotor system
                         for ir in range(len(prob_full)):
                             prob_full[ir]*=self.rhoVij[ir,path_phi[self.N-1,p]]
-                    if (i==(self.N-1)):
-                        # Periodic BC for the rightmost rotor
+                    if (i==(self.N-1)) and (self.N>2):
+                        # Periodic BC for the rightmost rotor, doesn't apply to the 2 rotor system
                         for ir in range(len(prob_full)):
                             prob_full[ir]*=self.rhoVij[ir,path_phi[0,p]]
         
@@ -737,13 +740,13 @@ class ChoiceMC(object):
                             if (i>0):
                                 # Only look at left neighbour to avoid double counting
                                 V_arr[n] += Vij(path_phi[i-1,p]*self.delta_phi, path_phi[i,p]*self.delta_phi, self.g)
-                                if (i==(self.N-1)):
+                                if (i==(self.N-1)) and (self.N>2):
                                     # Periodic BCs
                                     V_arr[n] += Vij(path_phi[i,p]*self.delta_phi, path_phi[0,p]*self.delta_phi, self.g)
                         if n >= self.Nequilibrate and orientationalCorrelations == True:
                             if (i>0):
                                 eiej_arr[n] += calculateOrientationalCorrelations(path_phi[i-1,p]*self.delta_phi, path_phi[i,p]*self.delta_phi)
-                                if (i==(self.N-1)):
+                                if (i==(self.N-1)) and (self.N>2):
                                     eiej_arr[n] += calculateOrientationalCorrelations(path_phi[i,p]*self.delta_phi, path_phi[0,p]*self.delta_phi)
                                     
                 # End of bead loop
@@ -757,7 +760,7 @@ class ChoiceMC(object):
                     if (i>0):
                         E_arr[n] += Vij(path_phi[i-1,0]*self.delta_phi, path_phi[i,0]*self.delta_phi, self.g)
                         E_arr[n] += Vij(path_phi[i-1,self.P-1]*self.delta_phi, path_phi[i,self.P-1]*self.delta_phi, self.g)
-                        if (i==(self.N-1)):
+                        if (i==(self.N-1)) and (self.N>2):
                             E_arr[n] += Vij(path_phi[i,0]*self.delta_phi, path_phi[0,0]*self.delta_phi, self.g)
                             E_arr[n] += Vij(path_phi[i,self.P-1]*self.delta_phi, path_phi[0,self.P-1]*self.delta_phi, self.g)
                 
@@ -774,51 +777,34 @@ class ChoiceMC(object):
         traj_out.close()
         
         if averagePotential == True:
-            V_average = np.mean(V_arr[self.Nequilibrate:])/self.N
-            V_stdError = np.std(V_arr[self.Nequilibrate:])/np.sqrt(self.MC_steps-self.Nequilibrate)/self.N
-            meanV, stdErrV = calculateError_byBinning(V_arr[self.Nequilibrate:]/self.N)
+            Ninteractions = self.N if self.N>2 else 1
+            meanV, stdErrV = calculateError_byBinning(V_arr[self.Nequilibrate:]/Ninteractions)
             if not self.PIGS:
-                V_average /= self.P
-                V_stdError /= self.P
                 meanV /= self.P
                 stdErrV /= self.P
-            print('<V> = ', V_average)
-            print('<V>_bin = ', meanV)
-            print('V_SE = ', V_stdError)
-            print('V_SE_bin = ', stdErrV)
-            self.V_MC = V_average
-            self.V_stdError_MC = V_stdError
-            self.V_MC_bin = meanV
-            self.V_stdError_MC_bin = stdErrV
+            print('<V> = ', meanV)
+            print('V_SE = ', stdErrV)
+            self.V_MC = meanV
+            self.V_stdError_MC = stdErrV
         if averageEnergy == True:
+            Ninteractions = self.N if self.N>2 else 1
             # Need to divide by two according to the PIGS formula
             E_arr/=2
-            E_average = np.mean(E_arr[self.Nequilibrate:])/self.N
-            E_stdError = np.std(E_arr[self.Nequilibrate:])/np.sqrt(self.MC_steps-self.Nequilibrate)/self.N
-            meanE, stdErrE = calculateError_byBinning(E_arr[self.Nequilibrate:]/self.N)
-            print('E0 = ', E_average)
-            print('E0_bin = ', meanE)
-            print('E0_SE = ', E_stdError)
-            print('E0_SE_bin = ', stdErrE)
-            self.E_MC = E_average
-            self.E_stdError_MC = E_stdError
-            self.E_MC_bin = meanE
-            self.E_stdError_MC_bin = stdErrE
+            meanE, stdErrE = calculateError_byBinning(E_arr[self.Nequilibrate:]/Ninteractions)
+            print('E0 = ', meanE)
+            print('E0_SE = ', stdErrE)
+            self.E_MC = meanE
+            self.E_stdError_MC = stdErrE
         if orientationalCorrelations == True:
-            eiej_average = np.mean(eiej_arr[self.Nequilibrate:])/self.N
-            eiej_stdError = np.std(eiej_arr[self.Nequilibrate:])/np.sqrt(self.MC_steps-self.Nequilibrate)/self.N
-            meanO, stdErrO = calculateError_byBinning(eiej_arr[self.Nequilibrate:]/self.N)
+            Ninteractions = self.N if self.N>2 else 1
+            meaneiej, stdErreiej = calculateError_byBinning(eiej_arr[self.Nequilibrate:]/Ninteractions)
             if not self.PIGS:
-                eiej_average /= self.P
-                eiej_stdError /= self.P
-            print('<ei.ej> = ', eiej_average)
-            print('<ei.ej>_bin = ', meanO)
-            print('ei.ej_SE = ', eiej_stdError)
-            print('ei.ej_SE_bin = ', stdErrO)
-            self.eiej_MC = eiej_average
-            self.eiej_stdError_MC = eiej_stdError
-            self.eiej_MC_bin = meanO
-            self.eiej_stdError_MC_bin = stdErrO
+                meaneiej /= self.P
+                stdErreiej /= self.P
+            print('<ei.ej> = ', meaneiej)
+            print('ei.ej_SE = ', stdErreiej)
+            self.eiej_MC = meaneiej
+            self.eiej_stdError_MC = stdErreiej
 
         self.histo = np.zeros((self.Ngrid,6))
         histo_out=open(os.path.join(self.path,'histo_A_P'+str(self.P)+'_N'+str(self.N)),'w')
@@ -1012,8 +998,8 @@ class ChoiceMC(object):
                             for ir in range(len(prob_full)):
                                 prob_full[ir]*=self.rhoVij[ir,path_phi[i-1,p]]
                                 prob_full_replica[ir]*=self.rhoVij[ir,path_phi_replica[i-1,p]] 
-                    if (i==0):
-                        # Periodic BC for the leftmost rotor
+                    if (i==0) and (self.N>2):
+                        # Periodic BC for the leftmost rotor, only applies for more than 2 rotors
                         if (p==P_middle) and swapped:
                             # Swaps the left interaction for the middle bead of the leftmost rotor
                             for ir in range(len(prob_full)):
@@ -1025,8 +1011,8 @@ class ChoiceMC(object):
                             for ir in range(len(prob_full)):
                                 prob_full[ir]*=self.rhoVij[ir,path_phi[self.N-1,p]]
                                 prob_full_replica[ir]*=self.rhoVij[ir,path_phi_replica[self.N-1,p]]
-                    if (i==(self.N-1)):
-                        # Periodic BC for the rightmost rotor
+                    if (i==(self.N-1)) and (self.N>2):
+                        # Periodic BC for the rightmost rotor, only applies for more than 2 rotors
                         if (p==P_middle) and swapped:
                             # Swaps the left interaction for the middle bead of the rightmost rotor
                             for ir in range(len(prob_full)):
@@ -1093,11 +1079,12 @@ class ChoiceMC(object):
                 rhoSwapped *= p_dist[path_phi_replica[i,P_midLeft-1],path_phi_replica[i,P_midLeft],path_phi[i,P_middle]]
                 
             # Potential contribution, this only impacts the middle bead
-            # Interactions for the periodic BCs
-            rhoUnswapped *= rhoVij_half[path_phi[0,P_middle],path_phi[self.N-1,P_middle]]
-            rhoUnswapped *= rhoVij_half[path_phi_replica[0,P_middle],path_phi_replica[self.N-1,P_middle]]
-            rhoSwapped *= rhoVij_half[path_phi_replica[0,P_middle],path_phi[self.N-1,P_middle]]
-            rhoSwapped *= rhoVij_half[path_phi[0,P_middle],path_phi_replica[self.N-1,P_middle]]
+            # Interactions for the periodic BCs, only applies for more than 2 rotors
+            if (self.N>2):
+                rhoUnswapped *= rhoVij_half[path_phi[0,P_middle],path_phi[self.N-1,P_middle]]
+                rhoUnswapped *= rhoVij_half[path_phi_replica[0,P_middle],path_phi_replica[self.N-1,P_middle]]
+                rhoSwapped *= rhoVij_half[path_phi_replica[0,P_middle],path_phi[self.N-1,P_middle]]
+                rhoSwapped *= rhoVij_half[path_phi[0,P_middle],path_phi_replica[self.N-1,P_middle]]
             # Interactions at the partition between A and B
             rhoUnswapped *= rhoVij_half[path_phi[N_partition-1,P_middle],path_phi[N_partition,P_middle]]
             rhoUnswapped *= rhoVij_half[path_phi_replica[N_partition-1,P_middle],path_phi_replica[N_partition,P_middle]]
@@ -1130,12 +1117,10 @@ class ChoiceMC(object):
         err_purity = abs(purity) * np.sqrt((errUnswapped/meanUnswapped)**2 + (errSwapped/meanSwapped)**2)
         entropy = -np.log(purity)
         err_entropy  = abs(err_purity)/purity
-        self.S2_binning = entropy
+        self.S2 = entropy
         self.S2_err = err_entropy
         
-        self.S2 = -np.log(N_swapped/N_unswapped)
         print('S2 = ', str(self.S2))
-        print('S2 Binning = ', str(self.S2_binning))
         
         self.histo = np.zeros((self.Ngrid,6))
         histo_out=open(os.path.join(self.path,'histo_A_P'+str(self.P)+'_N'+str(self.N)),'w')

Parametric_Sweeps/ChoiceMC_Sweep_Beta.py
@@ -39,7 +39,6 @@ for tau in tau_sweep:
 
     # Creating dictionaries to store the results from the g and N sweep
     g_sweep_dict_E = {}
-    g_sweep_dict_E_bin = {}
     # Performing the sweep over mMax
     for ig, g in enumerate(g_sweep):
         print("------------------------------------------------")
@@ -48,8 +47,6 @@ for tau in tau_sweep:
         # Creating arrays to store the E, V and O versus g data
         energy = np.zeros((len(T_sweep),3), float)
         energy_out = open(os.path.join(data_path,"Energy_g" + str(g) + '.dat'),'w')
-        energy_bin = np.zeros((len(T_sweep),3), float)
-        energy_out_bin = open(os.path.join(data_path,"EnergyBin_g" + str(g) + '.dat'),'w')
     
         for i, T in enumerate(T_sweep):
             print("------------------------------------------------")
@@ -70,8 +67,6 @@ for tau in tau_sweep:
             # Storing and saving the data from the current run
             energy[i,:] = [PIMC.beta, PIMC.E_MC, PIMC.E_stdError_MC]
             energy_out.write(str(PIMC.beta) + ' ' + str(PIMC.E_MC) + ' ' + str(PIMC.E_stdError_MC) + '\n')
-            energy_bin[i,:] = [PIMC.beta, PIMC.E_MC_bin, PIMC.E_stdError_MC_bin]
-            energy_out_bin.write(str(PIMC.beta) + ' ' + str(PIMC.E_MC_bin) + ' ' + str(PIMC.E_stdError_MC_bin) + '\n')
                 
             # Closing the remaining open plots
             plt.close('all')
@@ -81,29 +76,14 @@ for tau in tau_sweep:
         E_ax.plot()
         E_ax.errorbar(energy[:,0], energy[:,1], energy[:,2], label='PIGS', fmt='.-', capsize=3)
         E_ax.set_xlabel(r'$\beta (K^{-1})$')
-        E_ax.set_ylabel(r'$E_0$')
+        E_ax.set_ylabel(r'$E_0$'+' Per Interaction')
         E_ax.annotate('N = ' + str(N) + '; g = ' + str(g), xy=(0.5, 0.95),  xycoords='axes fraction', horizontalalignment='center', verticalalignment='top')
         E_ax.minorticks_on()
-        E_ax.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
         E_fig.tight_layout()
         E_fig.savefig(os.path.join(data_path,"Energy_g" + str(g) + ".png"))
-        
-        # Plotting
-        E_fig, E_ax = plt.subplots(1, 1, figsize=(8,5))
-        E_ax.plot()
-        E_ax.errorbar(energy_bin[:,0], energy_bin[:,1], energy_bin[:,2], label='PIGS', fmt='.-', capsize=3)
-        E_ax.set_xlabel(r'$\beta (K^{-1})$')
-        E_ax.set_ylabel(r'$E_0$')
-        E_ax.annotate('N = ' + str(N) + '; g = ' + str(g), xy=(0.5, 0.95),  xycoords='axes fraction', horizontalalignment='center', verticalalignment='top')
-        E_ax.minorticks_on()
-        E_ax.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
-        E_fig.tight_layout()
-        E_fig.savefig(os.path.join(data_path,"EnergyBin_g" + str(g) + ".png"))
             
         g_sweep_dict_E.update({g: energy})
         energy_out.close()
-        g_sweep_dict_E_bin.update({g: energy_bin})
-        energy_out_bin.close()
         plt.close('all')
         
     # Plotting the energy versus g for varied mMax
@@ -117,7 +97,7 @@ for tau in tau_sweep:
             xlim = ax.get_xlim()
         else:
             ax.set_xlim(xlim)
-        ax.set_ylabel(r'$E_0$')
+        ax.set_ylabel(r'$E_0$'+' Per Interaction')
         ax.minorticks_on()
         ax.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
         if i+1 == 2*((len(g_sweep)+1)//2) or i+1 == 2*((len(g_sweep)+1)//2)-1:
@@ -125,26 +105,6 @@ for tau in tau_sweep:
     E_fig.suptitle(r'$\tau\ =\ $' + str(tau))
     E_fig.tight_layout()
     E_fig.savefig("Energy_gBetaSweep.png")
-    
-    # Plotting the energy versus g for varied mMax
-    E_fig = plt.figure(figsize=(8,3*((len(g_sweep)+1)//2)))
-    xlim = 0.
-    for i, g in enumerate(g_sweep_dict_E_bin):
-        ax = E_fig.add_subplot((len(g_sweep)+1)//2, 2, i+1)
-        ax.errorbar(g_sweep_dict_E_bin[g][:,0], g_sweep_dict_E_bin[g][:,1], g_sweep_dict_E_bin[g][:,2], label='PIGS', fmt='.-', capsize=3)
-        ax.annotate('N = ' + str(N) + '; g = ' + str(g), xy=(0.5, 0.95),  xycoords='axes fraction', horizontalalignment='center', verticalalignment='top')
-        if i == 0:
-            xlim = ax.get_xlim()
-        else:
-            ax.set_xlim(xlim)
-        ax.set_ylabel(r'$E_0$')
-        ax.minorticks_on()
-        ax.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
-        if i+1 == 2*((len(g_sweep)+1)//2) or i+1 == 2*((len(g_sweep)+1)//2)-1:
-            ax.set_xlabel(r'$\beta\ (K^{-1})$')
-    E_fig.suptitle(r'$\tau\ =\ $' + str(tau))
-    E_fig.tight_layout()
-    E_fig.savefig("EnergyBin_gBetaSweep.png")
     
     plt.close("all")
     

Parametric_Sweeps/ChoiceMC_Sweep_Entanglement.py
@@ -10,7 +10,7 @@ import time
 import numpy as np
 import os
 
-time_str = "EntanglementSweep-"+str(time.gmtime().tm_year)+'-'+str(time.gmtime().tm_mon)+'-'+str(time.gmtime().tm_mday)+'-h'+str(time.gmtime().tm_hour)
+time_str = "EntanglementSweep-"+str(time.gmtime().tm_year)+'-'+str(time.gmtime().tm_mon)+'-'+str(time.gmtime().tm_mday)
 path = os.path.join(os.getcwd(), time_str)
 try:
     os.mkdir(path)
@@ -19,7 +19,7 @@ except FileExistsError:
 os.chdir(path)
 
 # Setting up the variables to sweep over
-g_sweep = np.linspace(0.01, 2, 10)
+g_sweep = np.linspace(0.01, 4, 30)
 N=4
     
 # Creating arrays to store the S2 versus g data
@@ -31,7 +31,7 @@ for ig, g in enumerate(g_sweep):
     print("Starting g = " + str(g))
     
     # Creating a ChoiceMC object for the current iteration
-    PIMC = ChoiceMC(m_max=5, P=9, g=g, MC_steps=100000, N=N, PIGS=True, Nskip=100, Nequilibrate=100)
+    PIMC = ChoiceMC(m_max=5, P=9, g=g, MC_steps=1000000, N=N, PIGS=True, Nskip=100, Nequilibrate=100)
     # Creating the probability density matrix for each rotor
     PIMC.createFreeRhoMarx()
     # Creating the probability density matrix for nearest neighbour interactions
@@ -43,17 +43,16 @@ for ig, g in enumerate(g_sweep):
     PIMC.plotHisto('PIMC')
     
     # Storing and saving the data from the current run
-    entanglement[ig,:] = [g, PIMC.S2, PIMC.S2_binning, PIMC.S2_err]
-    entanglement_out.write(str(g) + ' ' + str(PIMC.S2) +  ' ' + str(PIMC.S2_binning) +  ' ' + str(PIMC.S2_err) + '\n')
+    entanglement[ig,:] = [g, PIMC.S2, PIMC.S2_err]
+    entanglement_out.write(str(g) + ' ' + str(PIMC.S2) + ' ' + str(PIMC.S2_err) + '\n')
     
     # Closing the remaining open plots
     plt.close('all')
 
 # Plotting
 S2_fig, S2_ax = plt.subplots(1, 1, figsize=(8,5))
-S2_ax.errorbar(entanglement[:,0], entanglement[:,2], entanglement[:,3], fmt='.-', capsize=3, label='binning')
-S2_ax.plot(entanglement[:,0], entanglement[:,1], label='original')
-S2_ax.legend()
+S2_ax.errorbar(entanglement[:,0], entanglement[:,1], entanglement[:,2], fmt='.-', capsize=3)
+
 S2_ax.minorticks_on()
 S2_ax.set_xlabel('g')
 S2_ax.set_ylabel(r'$S_2$')

Parametric_Sweeps/ChoiceMC_Sweep_Ng.py
@@ -6,7 +6,7 @@ except ModuleNotFoundError:
     sys.path.append('..')
     from ChoiceMC import ChoiceMC
 import matplotlib.pyplot as plt
-from matplotlib.ticker import FormatStrFormatter
+
 import numpy as np
 import time
 import os
@@ -17,7 +17,7 @@ parent_dir = os.getcwd()
 T = 2./3.
 N_sweep = [2, 4, 16, 64, 256, 1028]
 # These are chosen to sweep the tau values, to extrapolate and get a fit PIGS value
-P_sweep = [29, 15, 9, 7]
+P_sweep = [15, 9, 7, 5]
 N = 2
 
 if N == 2:
@@ -43,19 +43,15 @@ except FileExistsError:
 
 # Creating dictionaries to store the results from the g sweep
 g_sweep_dict_E = {}
-g_sweep_dict_E_bin = {}
 g_sweep_dict_O = {}
-g_sweep_dict_O_bin = {}
 
 # Creating arrays to store the fit E and O versus g data
 arrE_fit = np.zeros((len(g_sweep),2), float)
-arrE_fit_bin = np.zeros((len(g_sweep),2), float)
 arrO_fit = np.zeros((len(g_sweep),2), float)
-arrO_fit_bin = np.zeros((len(g_sweep),2), float)
+arrO_SmallestTau = np.zeros((len(g_sweep),2), float)
 E_fit_out = open("Energy_N" + str(N) + '.dat','w')
-E_fit_out_bin = open("EnergyBin_N" + str(N) + '.dat','w')
 O_fit_out = open("OrienCorr_N" + str(N) + '.dat','w')
-O_fit_out_bin = open("OrienCorrBin_N" + str(N) + '.dat','w')
+O_SmallestTau_out = open("OrienCorrSmallestTau_N" + str(N) + '.dat','w')
 
 for ig, g in enumerate(g_sweep):
     print("------------------------------------------------")
@@ -63,13 +59,9 @@ for ig, g in enumerate(g_sweep):
 
     # Creating arrays to store the E and O versus g data
     arrE = np.zeros((len(P_sweep),3), float)
-    arrE_bin = np.zeros((len(P_sweep),3), float)
     arrO = np.zeros((len(P_sweep),3), float)
-    arrO_bin = np.zeros((len(P_sweep),3), float)
     E_out = open(os.path.join(data_path,"Energy_g" + str(round(g,3)) + '.dat'),'w')
-    E_out_bin = open(os.path.join(data_path,"EnergyBin_g" + str(round(g,3)) + '.dat'),'w')
     O_out = open(os.path.join(data_path,"OrienCorr_g" + str(round(g,3)) + '.dat'),'w')
-    O_out_bin = open(os.path.join(data_path,"OrienCorrBin_g" + str(round(g,3)) + '.dat'),'w')
     
     # Performing the sweep over mMax
     for iP, P in enumerate(P_sweep):
@@ -91,42 +83,30 @@ for ig, g in enumerate(g_sweep):
         
         # Storing and saving the data from the current run
         arrE[iP,:] = [PIMC.tau, PIMC.E_MC, PIMC.E_stdError_MC]
-        arrE_bin[iP,:] = [PIMC.tau, PIMC.E_MC_bin, PIMC.E_stdError_MC_bin]
         arrO[iP,:] = [PIMC.tau, PIMC.eiej_MC, PIMC.eiej_stdError_MC]
-        arrO_bin[iP,:] = [PIMC.tau, PIMC.eiej_MC_bin, PIMC.eiej_stdError_MC_bin]
         E_out.write(str(PIMC.tau) + ' ' + str(PIMC.E_MC) + ' ' + str(PIMC.E_stdError_MC) + '\n')
-        E_out_bin.write(str(PIMC.tau) + ' ' + str(PIMC.E_MC_bin) + ' ' + str(PIMC.E_stdError_MC_bin) + '\n')
         O_out.write(str(PIMC.tau) + ' ' + str(PIMC.eiej_MC) + ' ' + str(PIMC.eiej_stdError_MC) + '\n')
-        O_out_bin.write(str(PIMC.tau) + ' ' + str(PIMC.eiej_MC_bin) + ' ' + str(PIMC.eiej_stdError_MC_bin) + '\n')
             
         # Closing the remaining open plots
         plt.close('all')
     
     # Updating the dictionaries to store the raw results
     g_sweep_dict_E.update({g: arrE})
-    g_sweep_dict_E_bin.update({g: arrE_bin})
     g_sweep_dict_O.update({g: arrO})
-    g_sweep_dict_O_bin.update({g: arrO_bin})
     E_out.close()
-    E_out_bin.close()
     O_out.close()
-    O_out_bin.close()
     
     # Extrapolating to the tau = 0 limit
     Efit = np.polyfit(arrE[:,0], arrE[:,1], 2)
-    Efit_bin = np.polyfit(arrE_bin[:,0], arrE_bin[:,1], 2)
     Ofit = np.polyfit(arrO[:,0], arrO[:,1], 2)
-    Ofit_bin = np.polyfit(arrO_bin[:,0], arrO_bin[:,1], 2)
     
     # Storing the fitted results
     arrE_fit[ig, :] = [g, Efit[2]]
-    arrE_fit_bin[ig, :] = [g, Efit_bin[2]]
     arrO_fit[ig, :] = [g, Ofit[2]]
-    arrO_fit_bin[ig, :] = [g, Ofit_bin[2]]
+    arrO_SmallestTau[ig, :] = [g, arrO[arrO[:,0]==np.min(arrO[:,0])][0,1]]
     E_fit_out.write(str(g) + ' ' + str(Efit[2]) + '\n')
-    E_fit_out_bin.write(str(g) + ' ' + str(Efit_bin[2]) + '\n')
     O_fit_out.write(str(g) + ' ' + str(Ofit[2]) + '\n')
-    O_fit_out_bin.write(str(g) + ' ' + str(Ofit_bin[2]) + '\n')
+    O_SmallestTau_out.write(str(g) + ' ' + str(arrO[arrO[:,0]==np.min(arrO[:,0])][0,1]) + '\n')
     
     # Creating an array of tau for plotting a smooth line
     tauAx = np.linspace(0, np.max(arrE[:,0]), 40)
@@ -137,27 +117,13 @@ for ig, g in enumerate(g_sweep):
     ax.plot(0, Efit[2], marker='o', color='k', label='PIGS: Fit')
     ax.errorbar(arrE[:,0], arrE[:,1], arrE[:,2], label='PIGS', fmt='o', capsize=3)
     ax.set_xlabel(r'$\tau\ (K^{-1})$')
-    ax.set_ylabel(r'$E_0$')
+    ax.set_ylabel(r'$E_0$'+' Per Interaction')
     ax.set_xlim((0, ax.get_xlim()[1]))
     ax.annotate('N = ' + str(N) + '; g = ' + str(round(g,3)), xy=(0.5, 0.95),  xycoords='axes fraction', horizontalalignment='center', verticalalignment='top')
     ax.minorticks_on()
     ax.legend()
     fig.tight_layout()
     fig.savefig(os.path.join(data_path,"Energy_g" + str(round(g,3)) + ".png"))
-    
-    # Plotting E0 vs tau using the binning method
-    fig, ax = plt.subplots(1, 1, figsize=(8,5))
-    ax.plot(tauAx, (Efit_bin[0]*tauAx**2 + Efit_bin[1]*tauAx + Efit_bin[2]), color='k')
-    ax.plot(0, Efit_bin[2], marker='o', color='k', label='PIGS: Fit')
-    ax.errorbar(arrE_bin[:,0], arrE_bin[:,1], arrE_bin[:,2], label='PIGS', fmt='o', capsize=3)
-    ax.set_xlabel(r'$\tau\ (K^{-1})$')
-    ax.set_ylabel(r'$E_0$')
-    ax.set_xlim((0, ax.get_xlim()[1]))
-    ax.annotate('N = ' + str(N) + '; g = ' + str(round(g,3)), xy=(0.5, 0.95),  xycoords='axes fraction', horizontalalignment='center', verticalalignment='top')
-    ax.minorticks_on()
-    ax.legend()
-    fig.tight_layout()
-    fig.savefig(os.path.join(data_path,"EnergyBin_g" + str(round(g,3)) + ".png"))
     
     # Plotting O vs tau
     fig, ax = plt.subplots(1, 1, figsize=(8,5))
@@ -172,20 +138,6 @@ for ig, g in enumerate(g_sweep):
     ax.legend()
     fig.tight_layout()
     fig.savefig(os.path.join(data_path,"OrienCorr_g" + str(round(g,3)) + ".png"))
-    
-    # Plotting O vs tau using the binning method
-    fig, ax = plt.subplots(1, 1, figsize=(8,5))
-    ax.plot(tauAx, (Ofit_bin[0]*tauAx**2 + Ofit_bin[1]*tauAx + Ofit_bin[2]), color='k')
-    ax.plot(0, Ofit_bin[2], marker='o', color='k', label='PIGS: Fit')
-    ax.errorbar(arrO_bin[:,0], arrO_bin[:,1], arrO_bin[:,2], label='PIGS', fmt='o', capsize=3)
-    ax.set_xlabel(r'$\tau\ (K^{-1})$')
-    ax.set_ylabel("Orientational Correlation")
-    ax.set_xlim((0, ax.get_xlim()[1]))
-    ax.annotate('N = ' + str(N) + '; g = ' + str(round(g,3)), xy=(0.5, 0.95),  xycoords='axes fraction', horizontalalignment='center', verticalalignment='top')
-    ax.minorticks_on()
-    ax.legend()
-    fig.tight_layout()
-    fig.savefig(os.path.join(data_path,"OrienCorrBin_g" + str(round(g,3)) + ".png"))
 
     plt.close('all')
 
@@ -193,27 +145,16 @@ for ig, g in enumerate(g_sweep):
 fig, ax = plt.subplots(1, 1, figsize=(8,5))
 ax.plot(arrE_fit[:,0], arrE_fit[:,1], label='PIGS', marker='o', color='k')
 ax.set_xlabel('g')
-ax.set_ylabel(r'$E_0$')
+ax.set_ylabel(r'$E_0$'+' Per Interaction')
 ax.annotate('N = ' + str(N), xy=(0.5, 0.95),  xycoords='axes fraction', horizontalalignment='center', verticalalignment='top')
 ax.minorticks_on()
 ax.legend()
 fig.tight_layout()
 fig.savefig("Energy_N" + str(N) + ".png")
 
-# Plotting E0 versus g for the binning method
-fig, ax = plt.subplots(1, 1, figsize=(8,5))
-ax.plot(arrE_fit_bin[:,0], arrE_fit_bin[:,1], label='PIGS', marker='o', color='k')
-ax.set_xlabel('g')
-ax.set_ylabel(r'$E_0$')
-ax.annotate('N = ' + str(N), xy=(0.5, 0.95),  xycoords='axes fraction', horizontalalignment='center', verticalalignment='top')
-ax.minorticks_on()
-ax.legend()
-fig.tight_layout()
-fig.savefig("EnergyBin_N" + str(N) + ".png")
-
 # Plotting O versus g
 fig, ax = plt.subplots(1, 1, figsize=(8,5))
-ax.plot(arrO_fit[:,0], arrO_fit[:,1], label='PIGS', marker='o', color='k')
+ax.plot(arrO_fit[:,0], arrO_fit[:,1], label='PIGS: Fit', marker='o', color='k')
 ax.set_xlabel('g')
 ax.set_ylabel("Orientational Correlation")
 ax.annotate('N = ' + str(N), xy=(0.5, 0.95),  xycoords='axes fraction', horizontalalignment='left', verticalalignment='top')
@@ -222,21 +163,19 @@ ax.legend()
 fig.tight_layout()
 fig.savefig("OrienCorr_N" + str(N) + ".png")
 
-# Plotting O versus g for the binning method
+# Plotting O versus g
 fig, ax = plt.subplots(1, 1, figsize=(8,5))
-ax.plot(arrO_fit_bin[:,0], arrO_fit_bin[:,1], label='PIGS', marker='o', color='k')
+ax.plot(arrO_SmallestTau[:,0], arrO_SmallestTau[:,1], label='PIGS', marker='o', color='k')
 ax.set_xlabel('g')
 ax.set_ylabel("Orientational Correlation")
 ax.annotate('N = ' + str(N), xy=(0.5, 0.95),  xycoords='axes fraction', horizontalalignment='left', verticalalignment='top')
 ax.minorticks_on()
 ax.legend()
 fig.tight_layout()
-fig.savefig("OrienCorrBin_N" + str(N) + ".png")
+fig.savefig("OrienCorrSmallestTau_N" + str(N) + ".png")
 
 E_fit_out.close()
-E_fit_out_bin.close()
 O_fit_out.close()
-O_fit_out_bin.close()
 plt.close("all")
     
     